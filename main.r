2b4be5773bc16fc2f07ab46e55ea56696ada252b
lloydrichards <lloyd.d.richards@gmail.com>
2024-03-09 22:50:21 +0100

    feat(baumkataster): :sparkles: move over analysis for baumkataster

--- main.r
+++ main.r
@@ -5,13 +5,25 @@ library(ggplot2);
 # Load data
 data <- read.csv("data/baumkataster.csv");
 
-# Summarize data
-summary(data);
-head(data);
+# Turn data into a tibble
+raw_tibble <- as_tibble(data);
 
-# Plot data
-ggplot(data, aes(x=pflanzjahr)) +
-  geom_histogram(binwidth=1, fill="blue", color="black") +
-  theme_tufte();
+tidy_data <- raw_tibble %>%
+    mutate(poi_id,
+    status = as.factor(status),
+    baumtyp = as.factor(baumtyp),
+    quartier = as.factor(quartier),
+    pflanzjahr = as.Date(ISOdate(pflanzjahr,1,1)),
+    baumgattunglat = as.factor(baumgattunglat),
+    baumartlat = as.factor(baumartlat),
+    kategorie,
+    )
 
+# examine structure
+str(tidy_data)
 
+# how many trees are there of each species? max - 4747
+tidy_data %>%
+group_by(baumgattunglat, baumartlat)%>%
+summarize(count= n())%>%
+arrange(desc(count))
